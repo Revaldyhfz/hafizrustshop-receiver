651d4428ea776fcdc693aa5df8db54945bc0c895
Revaldyhfz <revaldyhafiz16@gmail.com>
2024-04-16 13:27:28 +0700

    Fixing typos

--- src/model/notification.rs
+++ src/model/notification.rs
@@ -17,11 +17,11 @@ impl Display for Notification {
         if self.status.to_uppercase().eq("CREATED") {
             return write!(f, 
             "Hello {}, let's try our new {} product: {}, only in HafizShop! Check it out: {}",
-        self.subscriber_name, self.product_type.to_lowercase(), self product_title, self.product_url);
+        self.subscriber_name, self.product_type.to_lowercase(), self.product_title, self.product_url);
         } else if self.status.to_uppercase().eq("DELETED") {
             return write!(f,
             "Hellow {}, we informed that out {} product called {} already sold out...",
-        self.subscriber_name, self.product_type.to_lowercase(), slef.product_title);
+        self.subscriber_name, self.product_type.to_lowercase(), self.product_title);
         } else {
             return write!(f, "Hello {}, let's try our {} product: {}, grab it out before the stock ran out! Check it out: {}",
             self.subscriber_name, self.product_type.to_lowercase(), self.product_title, self.product_url);
